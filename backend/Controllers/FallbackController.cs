d0709586bcc58886d28209b62243b2b4cdf60f1d
lakmalpasindu <nanayakk-cs18027@stu.kln.ac.lk>
2025-07-10 21:16:16 +0530

    fix: :ambulance: quick fixes

---
+++ backend/Controllers/FallbackController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace backend.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/browser", "index.html"), "text/HTML");
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -126,6 +126,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// is going to look for index.html and serving our api application using index.html
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
+
+
 #region ContextSeed
 using var scope = app.Services.CreateScope();
 try
